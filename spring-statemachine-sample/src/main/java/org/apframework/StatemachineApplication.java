205d646420be8ecc87c126911ce0b71a36cce267
liangwenxing <278496708@qq.com>
2019-03-27 22:26:32 +0800

    bugfix

--- spring-statemachine-sample/src/main/java/org/apframework/StatemachineApplication.java
+++ spring-statemachine-sample/src/main/java/org/apframework/StatemachineApplication.java
@@ -1,7 +1,5 @@
 package org.apframework;
 
-import org.apframework.fsm.NegotiationEvents;
-import org.apframework.fsm.NegotiationStateMachineService;
 import org.apframework.statemachine.StatemachineService;
 import org.apframework.statemachine.TurnstileEvents;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +12,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * @Author: Shoukai Huang
+ * @Author: wenxing.liang
  * @Date: 2018/9/28 21:12
  */
 @SpringBootApplication
@@ -22,9 +20,6 @@ public class StatemachineApplication implements CommandLineRunner {
 
     @Autowired
     private StatemachineService statemachineService;
-
-    /*@Autowired
-    private NegotiationStateMachineService negotiationStateMachineService;*/
 
     public static void main(String[] args) {
         SpringApplication.run(StatemachineApplication.class, args);
@@ -40,20 +35,6 @@ public class StatemachineApplication implements CommandLineRunner {
         statemachineService.execute(1, TurnstileEvents.PUSH, context);
         statemachineService.execute(1, TurnstileEvents.COIN, context);
         statemachineService.execute(1, TurnstileEvents.COIN, context);
-
-        //statemachineService.execute(1L, NegotiationEvents.CARRIER_FORBID);
-
-        /*for (int i=0;i<10;i++){
-            new Thread(new Runnable() {
-                @Override
-                public void run() {
-                    boolean execute = negotiationStateMachineService.execute(1L, NegotiationEvents.EXPIRE);
-                    if(!execute){
-                        throw new RuntimeException("出错了");
-                    }
-                }
-            }).run();
-        }*/
     }
 
 }
